6b1773659b02ff1a98f01fde7c7f07de75744e91
ramonrius <ramonrius@infojobs.net>
2017-04-24 17:33:17 +0200

    Add check to overwrite docker-compose.yml if intructed to

--- setup-zuul.sh
+++ setup-zuul.sh
@@ -3,3 +3,15 @@
 curl -L https://github.com/scm-spain/zuul-tools/raw/master/tar/zuul.tar.gz | tar zx
 
 curl -L -O https://github.com/scm-spain/zuul-tools/raw/master/docker-compose-zuul.yml
+
+if [ -f docker-compose.yml ]; then
+	echo "You already have a docker-compose.yml file. You want the new docker compose to overwrite this file?  (If select 'No' a new docker-compose-zuul.yml will be created)"	
+	select yn in "Yes" "No"; do
+    		case $yn in
+        		Yes ) mv -f docker-compose-zuul.yml docker-compose.yml; break;;
+        		No ) echo "Leaving docker-compose-zuul.yml as is"; exit;;
+    		esac
+	done
+else
+         mv -f docker-compose-zuul.yml docker-compose.yml;
+fi
