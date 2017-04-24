ecfeba7167f28b27ddc0a9232faa19350d5c2910
ramonrius <ramonrius@infojobs.net>
2017-04-24 16:42:25 +0200

    Add follow redirects to curl and change URL

--- setup-zuul.sh
+++ setup-zuul.sh
@@ -2,4 +2,4 @@
 
 curl -L https://github.com/scm-spain/zuul-tools/raw/master/tar/zuul.tar.gz | tar zx
 
-curl -O https://raw.githubusercontent.com/scm-spain/zuul-tools/master/docker-compose-zuul.yml
+curl -L -O https://github.com/scm-spain/zuul-tools/raw/master/docker-compose-zuul.yml
