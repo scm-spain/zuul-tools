c6d3d4227a5dd361142aad0214380f38caa97cb1
ramonrius <ramonrius@infojobs.net>
2017-04-24 16:32:49 +0200

    Zuul setup script

---
+++ setup-zuul.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -L https://github.com/scm-spain/zuul-tools/raw/master/tar/zuul.tar.gz | tar zx
+
+curl -O https://raw.githubusercontent.com/scm-spain/zuul-tools/master/docker-compose-zuul.yml
